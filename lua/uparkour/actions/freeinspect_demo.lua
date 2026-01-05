c7fc4e89144f813d74fe9c5446f61fe882e8651e
2016killer <ava741963@163.com>
2026-01-05 20:02:04 +0800

    补充完整目录结构

---
+++ lua/uparkour/actions/freeinspect_demo.lua
@@ -0,0 +1,10 @@
+
+local effect = UPEffect:Register('freeinspect_demo', 'default', {
+	label = '#default', 
+	AAAACreat = '白狼',
+	UPManip_hand_anim = 'models/player/Group03/male_03.mdl',
+    UPManip_hand_model = 'models/player/Group03/male_03.mdl',
+})
+
+
+if SERVER then return end
---
+++ lua/uparkour/effects/freeinspect_demo_eff.lua
@@ -0,0 +1,10 @@
+
+local effect = UPEffect:Register('freeinspect_demo', 'default', {
+	label = '#default', 
+	AAAACreat = '白狼',
+	UPManip_hand_anim = 'models/upmanip_demo/Group03/male_03.mdl',
+    UPManip_hand_model = 'models/upmanip_demo/Group03/male_03.mdl',
+})
+
+
+if SERVER then return end
